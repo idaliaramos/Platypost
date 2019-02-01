f90e9f3d13d24e00a299fb08d65ae574559f26a0
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2019-01-31 17:53:39 -0800

    added postcard config

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import { persistStore } from 'redux-persist';
-import ReduxThunk from 'redux-thunk';
-import rootReducer from '../redux/reducers/index'; // the value from combineReducers
+import { persistStore } from 'redux-persist'
+import rootReducer from '../redux/reducers/index' // the value from combineReducers
 import rootSaga from '../redux/sagas'
 
 const sagaMiddleware = createSagaMiddleware()
-export const store = createStore(rootReducer, {}, applyMiddleware(sagaMiddleware));
-export const persistor = persistStore(store);
+export const store = createStore(
+  rootReducer,
+  {},
+  applyMiddleware(sagaMiddleware)
+)
+export const persistor = persistStore(store)
 sagaMiddleware.run(rootSaga)
