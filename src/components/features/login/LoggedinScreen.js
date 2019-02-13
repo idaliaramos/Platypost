7c215046c8abc51a9cb52a9167e59fc15fdec71c
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2019-02-12 17:30:32 -0800

    added image picker and resized

--- src/components/features/login/LoggedinScreen.js
+++ src/components/features/login/LoggedinScreen.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
 import { Text, View, Image } from 'react-native'
-
-import ImagePicker from 'react-native-image-picker'
+import { connect } from 'react-redux'
+// import ImagePicker from 'react-native-image-picker'
+import ImagePicker from 'react-native-image-crop-picker'
 import Button from '../../common/Button'
-import Container from '../../common/Container'
+import CenteredContainer from '../../common/CenteredContainer'
 import * as NavigationService from '../../../../NavigationService'
 import * as postcardConstants from '../../../constants/create_postcard/PostcardConstants'
-
-ImagePicker
+import { addImage } from '../../../redux/actions/create_postcard'
 
 class LoggedinScreen extends Component {
   state = {
     image: ''
   }
 
-  // onSubmit = () => {
-  //   NavigationService.navigate('ADD_ADDRESS_1')
-  // }
+  onSubmit = () => {
+    const { addImage } = this.props
+    const { image } = this.state
+
+    addImage(image)
+    NavigationService.navigate('ADD_ADDRESS_1')
+  }
 
   // More info on all the options is below in the API Reference... just some common use cases shown here
 
@@ -24,48 +28,50 @@ class LoggedinScreen extends Component {
    * The first arg is the options object for customization (it can also be null or omitted for default options),
    * The second arg is the callback which sends object: response (more info in the API Reference)
    */
-  onSubmit = () => {
-    console.log('before', JSON.stringify(ImagePicker.showImagePicker))
-    console.log()
-    ImagePicker
-      ? ImagePicker.showImagePicker(null, () => {
-          console.log('inside')
-        })
-      : console.log('hello')
-    console.log()
-    console.log('after')
-    // response => {
-    //   console.log('Response = ', response)
-    //
-    //   if (response.didCancel) {
-    //     console.log('User cancelled image picker')
-    //   } else if (response.error) {
-    //     console.log('ImagePicker Error: ', response.error)
-    //   } else if (response.customButton) {
-    //     console.log('User tapped custom button: ', response.customButton)
-    //   } else {
-    //     const source = { uri: response.uri }
-    //
-    //     // You can also display the image using data:
-    //     // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-    //
-    //     this.setState({
-    //       avatarSource: source
-    //     })
-    //   }
-    // })
+  onUploadImage = () => {
+    ImagePicker.openPicker({
+      width: 500,
+      height: 400,
+      cropping: true,
+      includeBase64: true,
+      writeTempFile: false
+    }).then(image => {
+      // console.log(image.data, 'image')
+      // console.log(`data:${image.mime};base64,${image.data}`)
+      this.setState({
+        image
+      })
+    })
   }
 
   render() {
-    console.log(JSON.stringify(ImagePicker.showImagePicker), 'imagepicker')
+    const { image } = this.state
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={{ alignItems: 'center' }}>
         <Text>{postcardConstants.UPLOAD_IMAGE}</Text>
-        <Image source={this.state.avatarSource} />
-        <Container />
-        <Button onPress={this.onSubmit}>{postcardConstants.NEXT}</Button>
+        <Image
+          style={{ width: 500, height: 300, backgroundColor: 'grey' }}
+          source={{
+            uri: `data:${image.mime};base64,${image.data}`
+          }}
+        />
+        <Button onPress={this.onUploadImage}>
+          {!image ? 'upload image' : 'change image'}
+        </Button>
+        {image ? <Button onPress={this.onSubmit}>Next</Button> : null}
       </View>
     )
   }
 }
-export default LoggedinScreen
+// Do I need this? I dont thinks so but i need it for connect?
+mapStateToProps = state => {
+  const { image } = state.postCard
+
+  return {
+    image
+  }
+}
+export default connect(
+  mapStateToProps,
+  { addImage }
+)(LoggedinScreen)
