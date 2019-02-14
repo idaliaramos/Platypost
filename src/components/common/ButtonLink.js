9ef142849f8c2b94f94ecc68e145c3136fa41bb3
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2019-02-14 14:10:10 -0800

    created buttonlink

---
+++ src/components/common/ButtonLink.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, TouchableOpacity } from 'react-native'
+import styled, { css } from 'styled-components'
+
+const ButtonContainer = styled.TouchableOpacity`
+  /* background-color: #00cc99; */
+  /* width: 80%;
+  height: 40px; */
+  border-radius: 18px;
+  align-items: center;
+  justify-content: center;
+  margin-top: 25px;
+  text-decoration-line: underline;
+`
+const ButtonLink = ({ children, onPress, disabled }) => (
+  <ButtonContainer onPress={onPress}>
+    <Text>{children}</Text>
+  </ButtonContainer>
+)
+export default ButtonLink
