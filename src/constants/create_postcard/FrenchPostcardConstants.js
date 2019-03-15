921ce735da06d838329cfad3470974f532cf1862
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2019-03-14 23:49:33 -0700

    added french constants

---
+++ src/constants/create_postcard/FrenchPostcardConstants.js
@@ -0,0 +1,7 @@
+export const PAYMENT = 'Paiement';
+export const PAY = 'Pay';
+export const ENTER_MESSAGE = 'Entrer un message';
+export const NEXT = 'Next';
+export const SENDER_ADDRESS = 'Adresse';
+export const RECEIVER_ADDRESS = 'À';
+export const UPLOAD_IMAGE = 'télécharger une image';
