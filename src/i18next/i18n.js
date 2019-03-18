9a61f0b1577b2e86d8e52cf95f1f5d64aa621140
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2019-03-18 11:07:30 -0700

    edited translation

--- src/i18next/i18n.js
+++ src/i18next/i18n.js
@@ -11,19 +11,21 @@ I18n.fallbacks = true;
 // Define the supported translations
 I18n.translations = {
   en,
-  he,
   fr,
-  hun
+  hun,
+  span,
+  german,
+  chinese,
 };
 
 const currentLocale = I18n.currentLocale();
+//if no ,add default to english findBestAvailableLanguage
 
 // Is it a RTL language?
 export const isRTL = currentLocale.indexOf('he') === 0 || currentLocale.indexOf('ar') === 0;
 
 // Allow RTL alignment in RTL languages
 ReactNative.I18nManager.allowRTL(isRTL);
-I18n.locale = 'hun';
 // The method we'll use instead of a regular string
 export function strings(name, params = {}) {
   return I18n.t(name, params);
