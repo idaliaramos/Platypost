39c539b52468cde89c3de5c07a7d1fc2ac6bd30d
idaliaramos <IDALIARAMOS2@GMAIL.COM>
2019-03-29 03:11:52 -0700

    testing this converter

---
+++ src/components/features/login/pdfConverter.js
@@ -0,0 +1,18 @@
+import RNImageToPdf from "react-native-image-to-pdf";
+
+export default base64Arr => {
+  // It is a promise based function
+  // Create an array containing the path of each base64 images
+  let base64Paths = [];
+  base64Paths.length = 0; // re-initialize the array for further re-use
+
+  base64Arr.forEach(base64 => {
+    base64Paths.push(`data:image/jpeg;base64,${base64}`);
+  });
+
+  // Convert base64 images to pdf from the paths array
+  return RNImageToPdf.createPDFbyImages({
+    imagePaths: base64Paths,
+    name: "PDF_Name"
+  });
+};
